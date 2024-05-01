d28a380ad472d3d3ab594588c01c78b816d6a430
demuelle <dmueller@trilogyed.com>
2024-04-30 19:48:34 -0500

    makes a little changes so i know it's mine

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          YAY <code>src/App.jsx</code> and save to YAY HMR
         </p>
       </div>
       <p className="read-the-docs">
